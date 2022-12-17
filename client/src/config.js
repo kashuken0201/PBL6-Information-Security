c289071be0aa86dd3ac64281617bac385a184236
Tran Tan Phat <kashukiller@gmai.com>
2022-12-17 17:59:04 +0700

    remove

--- client/src/config.js
+++
@@ -1,1 +0,0 @@
-export default BASE_URL = 'http://192.168.1.10:8888'
--- client/src/utils/convert-date.js
+++
@@ -1,9 +0,0 @@
-const convertUTCDateToLocalDate = (date) => {
-    var newDate = new Date(date.getTime() + date.getTimezoneOffset() * 60 * 1000);
-    var offset = date.getTimezoneOffset() / 60;
-    var hours = date.getHours();
-    newDate.setHours(hours - offset);
-    return newDate;
-}
-
-export default convertUTCDateToLocalDate;
